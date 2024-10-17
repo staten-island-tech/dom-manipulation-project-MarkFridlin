219561c3f5e2ec14d76435a74e81c2dcc188925c
MarkFrid <158204105+MarkFridlin@users.noreply.github.com>
2024-10-17 12:27:57 -0400

    skibidi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const DOMSelctors = {
-  header: document.querySelector(".parent"),
-  description: document.querySelector("button"),
+  parentdiv: document.querySelector(".parent"),
+  submitButton: document.querySelector("button"),
   title: document.querySelector("#title"),
   desc: document.querySelector("#desc"),
 };
@@ -13,7 +13,39 @@ function removePrompt(num) {
 }
 function addElement(num) {
   DOMSelctors.parentdiv.insertAdjacentHTML(
-    "befreend",
-    `<div class="card" id="p${num}"><h4>${DOMSelctors.title.value}</h4><img src="${DOMSelctors.desc.value}"<button id="b${num}>Delete</div>`
+    "beforeend",
+    `<div class="card" id="p${num}"><h4>${DOMSelctors.title.value}</h4><img src="${DOMSelctors.desc.value}"><button id="b${num}>Delete</button></div>`
   );
+  document
+    .querySelector(`#b${num}`)
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.querySelector(`#p${num}`).remove();
+    });
+  function run() {
+    let n = 0;
+    DOMSelctors.submitButton.addEventListener("click", function (event) {
+      if (!(DOMSelctors.title.value === "" || DOMSelctors.desc.value === "")) {
+        event.preventDefault();
+        addElement(n);
+        clearInput();
+        n += 1;
+      }
+    });
+  }
 }
+[title, desc].forEach((input) => {
+  input.addEventListener("keypress", function (event) {
+    if (
+      !(DOMSelctors.title.value === "" || DOMSelctors.desc.value === "") &
+      (event.key === "enter")
+    ) {
+      event.preventDefault();
+      addElement(n);
+      clearInput();
+      n += 1;
+    }
+  });
+});
+
+run();
